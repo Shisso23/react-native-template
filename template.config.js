9ed15a6d662cc4f16a4d6676141cc7765093a382
Matthew Dalton <matt@codehesion.co.za>
2019-10-22 14:28:32 +0200

    Renamed template to `Codehesion`

--- template.config.js
+++ template.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    placeholderName: 'codehesion',
+    placeholderName: 'Codehesion',
     templateDir: './template'
 };
--- template/ios/codehesionTests/codehesionTests.m
+++ template/ios/codehesionTests/codehesionTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface codehesionTests : XCTestCase
+@interface CodehesionTests : XCTestCase
 
 @end
 
-@implementation codehesionTests
+@implementation CodehesionTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
